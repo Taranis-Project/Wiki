9c8232b58085e4007ceb31c558d5e9240a6bee7b
Taranis-Project <141488032+Taranis-Project@users.noreply.github.com>
2026-04-22 22:49:56 +0200

    Add Example4.m for PID control system analysis

---
+++ Example4.m
@@ -0,0 +1,33 @@
+clc 
+%clear all
+close all
+
+format long
+
+K=-1.8479;
+Tw=0.28017;
+Zeta=0.36929;
+
+s = tf('s');
+
+f=K/(1+(2*Zeta*Tw)*s+(Tw*s)^2)
+
+% Your PID definition
+PID = -1.9379 * (1 + 0.48*s + 0.0576*s^2) / s;
+
+Pe=0.01
+
+H = tf([1 -1],[1 4 5],'InputDelay', 0.3); 
+Hd = c2d(H,0.1,'foh');
+
+PID_d = c2d(PID, Pe, 'tustin')
+
+sisotool(f)
+CL=f*PID/(1+f*PID)
+
+figure(1)
+step(-f)
+
+figure(2)
+step(CL)
+
